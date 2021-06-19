953d19ef4d399f4ac2e861402c2d839c4b5d996f
Hugo L. Ruffo <hlruffo@gmail.com>
2021-06-19 18:54:22 -0300

    Palindromo
    Verifica se uma frase lida de tras para frente é igual a frase original.

---
+++ palindromo.py
@@ -0,0 +1,19 @@
+# coding: iso-8859-1 -*-
+
+repete = 'sim'
+while repete == 'sim':
+    frase = str(input('Digite uma frase para saber se é palindroma: ')).upper().strip()
+    separa = frase.split()
+    junto= ''.join(separa)
+    reverso =''
+    for letra in range(len(junto)-1,-1,-1):
+        reverso += junto[letra]
+    if reverso == junto:
+        print(f'A frase {frase} é palindroma pois sem os espaços fica: {junto} sendo igual ao ler de trás pra frente: {reverso}')
+    else:
+        print(f'A frase {frase} não é palindroma pois sem os espaços fica: {junto} sendo diferente de ao ler de trás pra frente: {reverso}')
+
+    repete = input ('Avaliar outra frase? Digite sim ou não: \n').lower ( )
+    if repete != 'sim' and repete != 'não':
+            repete = input('Opção inválida ? Digite sim ou não: \n').lower()
+print('Até a próxima!')
