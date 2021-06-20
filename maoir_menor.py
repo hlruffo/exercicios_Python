ac9cfc78b3b1558f52ce768639c429e1782248ed
Hugo L. Ruffo <hlruffo@gmail.com>
2021-06-19 21:33:07 -0300

    Update maoir_menor.py
    correção da validação de repete

--- maoir_menor.py
+++ maoir_menor.py
@@ -7,7 +7,7 @@ while repete == 'sim':
     menor =[]
     atual = date.today().year
     for numero in range(1,8):
-        nascimento = int(input(f'Digite o ano de nascimento da pessoa {numero} com 4 dÌgitos:'))
+        nascimento = int(input(f'Digite o ano de nascimento da pessoa {numero} com 4 d√≠gitos:'))
         idade = atual - nascimento
         if idade >= 18:
             maior.append(numero)
@@ -15,10 +15,10 @@ while repete == 'sim':
             menor.append(numero)
     maior_str = str(maior)[1:-1]
     menor_str = str(menor)[1:-1]
-    print(f'Na lista h· {len(maior)} pessoas maiores de idade, sendo elas {maior_str}')
-    print(f'Na lista h· {len(menor)} pessoas menores de idade, sendo elas {menor_str}')
+    print(f'Na lista h√° {len(maior)} pessoas maiores de idade, sendo elas {maior_str}')
+    print(f'Na lista h√° {len(menor)} pessoas menores de idade, sendo elas {menor_str}')
 
-    repete = input('Avaliar outro grupo? Digite sim ou n„o: \n').lower()
-    if repete != 'sim' and repete != 'n„o':
-            repete = input('OpÁ„o inv·lida ? Digite sim ou n„o: \n').lower()
-print('AtÈ a prÛxima!')
+    repete = input('Avaliar outro grupo? Digite sim ou n√£o: \n').lower()
+    while repete != 'sim' and repete != 'n√£o':
+            repete = input('Op√ß√£o inv√°lida ? Digite sim ou n√£o: \n').lower()
+print('At√© a pr√≥xima!')
