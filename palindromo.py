e822e44e8523aa59ebde7f8bfec9c56f9392f250
Hugo L. Ruffo <hlruffo@gmail.com>
2021-06-19 21:36:10 -0300

    Update palindromo.py
    correção no repete

--- palindromo.py
+++ palindromo.py
@@ -2,18 +2,18 @@
 
 repete = 'sim'
 while repete == 'sim':
-    frase = str(input('Digite uma frase para saber se é palindroma: ')).upper().strip()
+    frase = str(input('Digite uma frase para saber se Ã© palindroma: ')).upper().strip()
     separa = frase.split()
     junto= ''.join(separa)
     reverso =''
     for letra in range(len(junto)-1,-1,-1):
         reverso += junto[letra]
     if reverso == junto:
-        print(f'A frase {frase} é palindroma pois sem os espaços fica: {junto} sendo igual ao ler de trás pra frente: {reverso}')
+        print(f'A frase {frase} Ã© palindroma pois sem os espaÃ§os fica: {junto} sendo igual ao ler de trÃ¡s pra frente: {reverso}')
     else:
-        print(f'A frase {frase} não é palindroma pois sem os espaços fica: {junto} sendo diferente de ao ler de trás pra frente: {reverso}')
+        print(f'A frase {frase} nÃ£o Ã© palindroma pois sem os espaÃ§os fica: {junto} sendo diferente de ao ler de trÃ¡s pra frente: {reverso}')
 
-    repete = input ('Avaliar outra frase? Digite sim ou não: \n').lower ( )
-    if repete != 'sim' and repete != 'não':
-            repete = input('Opção inválida ? Digite sim ou não: \n').lower()
-print('Até a próxima!')
+    repete = input ('Avaliar outra frase? Digite sim ou nÃ£o: \n').lower ( )
+    while repete != 'sim' and repete != 'nÃ£o':
+            repete = input('OpÃ§Ã£o invÃ¡lida ? Digite sim ou nÃ£o: \n').lower()
+print('AtÃ© a prÃ³xima!')
