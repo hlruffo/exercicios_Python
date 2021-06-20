a5b2534ee62b8ecc08c7c8ed34648197e70b5f3e
Hugo L. Ruffo <hlruffo@gmail.com>
2021-06-20 17:41:32 -0300

    Progressão aritmética
    Dado um número e a razão informa os 10 primeiros números de uma PA

---
+++ PAwhile.py
@@ -0,0 +1,15 @@
+﻿repete = 'sim'
+while repete == 'sim':
+    inicio = int(input('Informe o primeiro termo da sua PA: '))
+    razao = int(input(('Informe a razão da PA: ')))
+    cont = 1
+    print(f'Os termos da PA com início em {inicio}  e razão {razao} são:')
+    while cont < 11:
+        print(f'{inicio}', end=' -> ')
+        inicio = inicio + razao
+        cont += 1
+    print ('Fim de sequência \n')
+    repete = input('Gostaria de avaliar outra lista? Digite sim ou não: ')
+    while repete != 'sim' and repete != 'não':
+        repete = input('Comando inválido. Digite sim ou não: ')
+print('Programa encerrado. Até a próxima.')
