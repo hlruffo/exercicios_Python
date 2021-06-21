dc8c4baafa9fafcc25960ba59d15a56bb600b2a7
Hugo L. Ruffo <hlruffo@gmail.com>
2021-06-21 15:36:30 -0300

    Tupla aleatória
    Obrigatório usar tupla.
    Gera um tupla com 5 números aleatórios e determina o maior e o menor entre eles.

---
+++ tuplaaleatoria.py
@@ -0,0 +1,15 @@
+﻿from random import randint
+repete = 'sim'
+while repete == 'sim':
+    numeros = (randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10))
+    print('Os números sorteados foram:')
+    for n in numeros:
+        print(f'{n}', end=' ')
+    print(f'. O maior valor é {max(numeros)} e o menor {min(numeros)}')
+
+    repete = input('Gostaria de sortear de novo? Digite sim ou não: ').lower()
+    while repete != 'sim' and repete != 'não':
+        repete = input('Comando inválido. Digite sim ou não: ')
+
+print('Programa encerrado. Até a próxima.')
+
