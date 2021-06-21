09f75e039b53337e1916a8f9521a44cecd35a319
Hugo L. Ruffo <hlruffo@gmail.com>
2021-06-21 15:06:12 -0300

    Análise de times
    Informa o top 5, z4 , times em oredm alfabética e posição do Fluminense.

---
+++ Analisetimes.py
@@ -0,0 +1,14 @@
+﻿times = ('América-MG', 'Athletico', 'Atlético-GO', 'Atlético-MG', 'Bahia', 'Bragantino', 'Ceará', 'Chapecoense',
+         'Corinthians', 'Cuiabá', 'Fortaleza', 'Flamengo', 'Fluminense', 'Grêmio', 'Internacional', 'Juventude',
+         'Palmeiras', 'Santos','São Paulo', 'Sport')
+print('-=' * 15)
+print('BRASILEIRÃO 2021')
+print('-=' * 15)
+print(f'Participam do campeonato: {sorted(times)}')
+print('-=' * 15)
+print(f'O TOP 5 possui: {times[0:5]}')
+print('-=' * 15)
+print(f'O Z4 possui: {times[-4:]}')
+print('-=' * 15)
+print(f'O Fluminense está na {times.index("Fluminense")+1}ª posição. ')
+print('-=' * 15)
