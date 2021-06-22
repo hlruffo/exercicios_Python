5bf7702781df29f62aa246d7a51a3fc444ce5ff3
Hugo L. Ruffo <hlruffo@gmail.com>
2021-06-22 11:04:26 -0300

    Tabela de preços
    Exercício com tupla. 
    usando apenas uma tupla formatar um tabela de preços, apresentada com duas colunas, uma para iten e outra para o preço.
    O exercício tem bastante detalhe de formatação para impressão

---
+++ tabpreco.py
@@ -0,0 +1,18 @@
+﻿itens =('Lápis', 1.75,
+         'Borracha', 2,
+         'Caderno', 15.90,
+         'Estojo', 25,
+         'Transferidor', 4.20,
+         'Compasso', 9.90,
+         'Mochila', 120.32,
+         'kit Caneta', 22.90,
+         'Livro', 34.90)
+print('-' * 40)
+print(f'{"Lista de preços" :^40}')
+print('-' * 40)
+for i in range(0,len(itens)):
+    if i % 2 ==0:
+        print(f'{itens[i]:.<30}', end='')
+    else:
+        print(f'R${itens[i]:>7.2f}')
+print('-' * 40)
